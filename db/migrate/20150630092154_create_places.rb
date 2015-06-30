5c43d317570711327ce5f488aa3104efe7b249bd
Faisal <chaudhary.fsl@gmail.com>
2015-06-30 09:28:53 +0000

    set up model for place, added db for three columns

---
+++ db/migrate/20150630092154_create_places.rb
@@ -0,0 +1,13 @@
+class CreatePlaces < ActiveRecord::Migration
+  def change
+    create_table :places do |t|
+
+    	t.string :name
+    	t.text :description
+    	t.string :address
+
+
+    	t.timestamps
+    end
+  end
+end
