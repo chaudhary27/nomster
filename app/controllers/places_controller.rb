616131b7b073c8ef320cbdd1cf6bd1e39c3f2cfd
Faisal <chaudhary.fsl@gmail.com>
2015-07-04 23:36:08 +0000

    added simple_form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,11 @@
 class PlacesController < ApplicationController
 	def index
 		@places = Place.all
-		
+		@places = Place.paginate(:page => params[:page], :per_page => 5)
 	end
+
+	def new
+		@place = Place.new
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Nomster::Application.routes.draw do
 
   root 'places#index'
+
+  resources :places
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
