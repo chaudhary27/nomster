f6ae0de1ab5bb97c1cb910cedf0fdef33d794ed4
Faisal <chaudhary.fsl@gmail.com>
2015-07-08 04:27:17 +0000

    updating the place with new data

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, :only => [:new, :create]
 	def index
 		@places = Place.all
-		@places = Place.paginate(:page => params[:page], :per_page => 5)
+		@places = Place.paginate(:page => params[:page], :per_page => 5).order('created_at ASC')
 	end
 
 	def new
@@ -16,6 +16,18 @@ class PlacesController < ApplicationController
 
 	def show
 		@place = Place.find(params[:id])
+	end
+
+	def edit
+		@place = Place.find(params[:id])
+		
+	end
+
+	def update
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
+		
 	end
 
 	private
