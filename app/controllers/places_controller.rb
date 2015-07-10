172676d793a2ae589ff9e816c191ccfc27530374
Faisal <chaudhary.fsl@gmail.com>
2015-07-10 05:14:49 +0000

    secured app for edit and update action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 	def index
 		@places = Place.all
 		@places = Place.paginate(:page => params[:page], :per_page => 5).order('created_at ASC')
@@ -20,11 +20,20 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 		
 	end
 
 	def update
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
+		
 		@place.update_attributes(place_params)
 		redirect_to root_path
 		
