26d22450e391462d5e10ddc2fb907d41f4992533
Faisal <chaudhary.fsl@gmail.com>
2015-07-10 06:22:03 +0000

    added security for addrs and desc also added name char rest

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, length: {minimum: 3}
+	validates :address, :presence =>true
+	validates :description, :presence => true
 end
