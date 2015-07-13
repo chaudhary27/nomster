f8129ac0b88cdb5f45393f1dc1ccbb3fb1de8144
Faisal <chaudhary.fsl@gmail.com>
2015-07-13 11:44:09 +0000

    created controller for comments to allow submit action on the form save the quotes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Nomster::Application.routes.draw do
   devise_for :users
   root 'places#index'
 
-  resources :places
+  resources :places do
+    resources :comments, :only => :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
