29a4afb007a2a43dfa4e2f3ac3562dfd75c59e98
Faisal <chaudhary.fsl@gmail.com>
2015-07-14 07:56:29 +0000

    added RATINGS hash table + humanized readable text in the ratings column

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+
+	RATINGS = {
+		'one star' => '1_star',
+		'two stars' => '2_star',
+		'three stars' => '3_stars',
+		'four stars' => '4_stars',
+		'five stars' => '5_stars'
+	}
+
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
 end
