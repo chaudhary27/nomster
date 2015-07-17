1d95efe57f0829b8c080df482f89e146b92744ef
Faisal <chaudhary.fsl@gmail.com>
2015-07-17 05:16:15 +0000

    adjusting table heroku run rake error

--- db/migrate/20150715064319_create_photos.rb
+++ db/migrate/20150715064319_create_photos.rb
@@ -2,13 +2,14 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.text :caption
-      t.integer :place_id	
+      t.integer :place_id
+      t.integer :user_id	
 
 
       t.timestamps
     end
 
-   # add_index :photos, [:user_id, :place_id]
-   # add_index :photos, :place_id
+   add_index :photos, [:user_id, :place_id]
+   add_index :photos, :place_id
   end
 end
