6a70680257d2d20e159fd7cbe20d064b400efa82
Faisal <chaudhary.fsl@gmail.com>
2015-07-21 22:27:27 +0000

    testing mailgun heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,15 @@
 Nomster::Application.configure do
 
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   config.action_mailer.default_url_options = { :host => 'nomster-faisal.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
