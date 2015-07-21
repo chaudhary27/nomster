6309b2b38a17749a1dcbe5b7c1694a4a52e69a20
Faisal <chaudhary.fsl@gmail.com>
2015-07-21 15:46:20 +0000

    testing notification email in development.rb

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-  	mail(to: "fsl_27@yahoo.com",
+  	mail(to: "chaudhary.fsl@gmail.com",
   		subject: "A Comment has been added to your place")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,17 @@
 Nomster::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3030' }
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -15,7 +27,7 @@ Nomster::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
