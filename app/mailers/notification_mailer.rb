f8b8061b51f25693775f4e16b8102563e76b7a60
Faisal <chaudhary.fsl@gmail.com>
2015-07-21 16:23:43 +0000

    adjusted automated email to configure to place and user

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-  	mail(to: "chaudhary.fsl@gmail.com",
-  		subject: "A Comment has been added to your place")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+  	mail(to: @place_owner.email,
+  		subject: "A Comment has been added to #{@place.name}")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ Nomster::Application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3030' }
 
   config.action_mailer.delivery_method = :smtp
-  
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
