d70ee4187ef77576b56f02cb77f84d73d625a20c
Faisal <chaudhary.fsl@gmail.com>
2015-07-22 07:48:01 +0000

    created controller for users, created route for show action and hooked up the view for user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Nomster::Application.routes.draw do
     resources :comments, :only => :create
     resources :photos, :only => :create
   end
+
+  resources :users, :only => :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
