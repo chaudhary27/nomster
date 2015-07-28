ec8b55d1ce9ae7dcd5f04e3f69abcc95f906cc0f
Faisal <chaudhary.fsl@gmail.com>
2015-07-28 02:49:23 +0000

    added comment_added test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "comment_added" do
+  	user = FactoryGirl.create(:user)
+  	sign_in user
+
+	  	place = FactoryGirl.create(:place)
+	  	comment = FactoryGirl.create(:comment)
+
+	  		assert_difference 'Comment.count' do
+			post :create, :place_id => place.id, :comment => {
+				message: comment.message,
+				rating: comment.rating
+			}
+			end
+
+		assert_equal 1, user.comments.count	
+  		assert_redirected_to place_path(place)
+  	end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user_dashboard" do
+  	user = FactoryGirl.create(:user)
+  	sign_in user
+  	get :show, :id => user.id
+  	assert_response :success
+  end
 end
